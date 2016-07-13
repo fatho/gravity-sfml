04c83822de3408e176fd3f7e70f9ceae50eef403
Fabian Thorand <f.thorand@gmail.com>
2016-07-13 17:51:40 +0200

    Catch all unhandled exceptions
    Any unhandled exception is caught in main and written to the log file.

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,20 @@
 #include "octo/game.hpp"
+#include "fmtlog/fmtlog.hpp"
+
 #include <SFML/Graphics.hpp>
+#include <boost/type_index.hpp>
 
 int main() {
-  octo::Game game;
-  game.run();
+  fmtlog::Log log("<main>");
+  log.info("started");
+  try {
+    octo::Game game;
+    game.run();
+  } catch(const std::exception& ex) {
+    log.fatal("unhandled exception of type %s: %s", boost::typeindex::type_id_runtime(ex).pretty_name(), ex.what());
+  } catch(...) {
+    log.fatal("unhandled unkown exception");
+  }
+  log.info("about to exit");
+  return 0;
 }
